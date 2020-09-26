05fe17b9ec4885c5b31390a03b2e3119274117c4
Lam Son Ho <holamson2000@gmail.com>
2020-09-26 16:23:13 +0700

    make scripts work

--- scripts/run_wsgi.sh
+++ scripts/run_wsgi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export FLASK_APP=home_fserver && export FLASK_ENV=production && uwsgi --socket 0.0.0.0:8090 --master --protocol=http -w wsgi:app --processes 6 --threads 2
+export FLASK_APP=home_fserver && export FLASK_ENV=production && uwsgi --socket 0.0.0.0:58000 --master --protocol=http -w wsgi:app --processes 6 --threads 2

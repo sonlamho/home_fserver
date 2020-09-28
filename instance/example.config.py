cdbc439523a6bc7210981023c19f16ea8157cbe2
Lam Son Ho <holamson2000@gmail.com>
2020-09-28 15:07:22 +0700

    add an example config file

---
+++ instance/example.config.py
@@ -0,0 +1,7 @@
+
+# Option to allow creation of a secret url path that do not require logging in
+# maybe useful to allow scripted download
+ALLOW_SECRET = True
+
+# Option to allow the delete form in the "Actions" section
+ALLOW_DELETE = True

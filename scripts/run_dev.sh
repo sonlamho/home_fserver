3d1162bf3fa8f6013303ed3387a30794374061de
Lam Son Ho <holamson2000@gmail.com>
2023-01-16 20:48:28 +0700

    specify port 5000 in dev

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export FLASK_APP=home_fserver && export FLASK_ENV=development && flask run --host=0.0.0.0
+export FLASK_APP=home_fserver && export FLASK_ENV=development && flask run --host=0.0.0.0 --port 5000
